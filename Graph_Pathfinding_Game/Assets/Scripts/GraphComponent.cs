7674c3e9d1f3fec6e5e258fab286f58a43f2cac3
E-AleynaElmas <a.aleynaelmas@gmail.com>
2021-05-24 17:55:22 +0300

    Created a graph structure and drew graph with gizmos.

---
+++ Graph_Pathfinding_Game/Assets/Scripts/GraphComponent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GraphComponent : MonoBehaviour
+{
+    private Graph<Vector2, float> graph;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        graph = new Graph<Vector2, float>();
+        var node1 = new Node<Vector2>() { Value = Vector2.zero, NodeColor = Color.red };
+        var node2 = new Node<Vector2>() { Value = Vector2.one, NodeColor = Color.cyan };
+        var edge1 = new Edge<float, Vector2>() { Value = 1.0f, From = node1, To = node2, EdgeColor = Color.yellow };
+
+        graph.Nodes.Add(node1);
+        graph.Nodes.Add(node2);
+
+        graph.Edges.Add(edge1);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(graph == null)
+        {
+            Start();
+        }
+        
+        foreach(var node in graph.Nodes)
+        {
+            Gizmos.color = node.NodeColor;
+            Gizmos.DrawSphere(node.Value, 0.125f);
+        }
+
+        foreach(var edge in graph.Edges)
+        {
+            Gizmos.color = edge.EdgeColor;
+            Gizmos.DrawLine(edge.From.Value, edge.To.Value);
+        }
+    }
+}
